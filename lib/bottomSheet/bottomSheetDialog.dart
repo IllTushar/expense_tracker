6b9c0858a5c6cc4364048cd6a73f4792060c4e66
IllTushar <gtushar7668@gmail.com>
2023-11-24 00:33:30 +0530

    24.11.23x1

--- lib/bottomSheet/bottomSheetDialog.dart
+++ lib/bottomSheet/bottomSheetDialog.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expense_model/expense.dart' as category;
+import 'package:google_fonts/google_fonts.dart';
 
 class BottomSheetDialog extends StatefulWidget {
   BottomSheetDialog();
@@ -11,11 +14,20 @@ class BottomSheetDialog extends StatefulWidget {
 class _BottomSheetDialogState extends State<BottomSheetDialog> {
   final _newText = TextEditingController();
   final _amount = TextEditingController();
+  DateTime? showDateTime;
+  category.Category _selectedCategory = category.Category.leisure;
 
-  void _datePicker() {
+  void _datePicker() async {
     final now = DateTime.now();
-    final firstDate = DateTime(now.year-1,now.month,now.day);
-    showDatePicker(context: context,initialDate: now, firstDate: firstDate, lastDate: now);
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickTheDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+    setState(() {
+      showDateTime = pickTheDate;
+    });
   }
 
   @override
@@ -29,7 +41,7 @@ class _BottomSheetDialogState extends State<BottomSheetDialog> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Padding(
-      padding: EdgeInsets.all(19.0),
+      padding: const EdgeInsets.all(19.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -48,7 +60,7 @@ class _BottomSheetDialogState extends State<BottomSheetDialog> {
                       prefix: Text('\$'), label: Text("Amount")),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 10,
               ),
               Expanded(
@@ -56,7 +68,9 @@ class _BottomSheetDialogState extends State<BottomSheetDialog> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("Selected Date"),
+                    Text(showDateTime == null
+                        ? "No Date Selected"
+                        : category.formatDates.format(showDateTime!)),
                     IconButton(
                         onPressed: _datePicker,
                         icon: Icon(Icons.calendar_month)),
@@ -65,11 +79,34 @@ class _BottomSheetDialogState extends State<BottomSheetDialog> {
               ),
             ],
           ),
-          ElevatedButton(
-              onPressed: () {
-                print(_newText.text);
-              },
-              child: const Text("Save And Continue"))
+          Row(
+            children: [
+              const SizedBox(height: 18,),
+              DropdownButton(
+                value: _selectedCategory,
+                items: category.Category.values.map((category) {
+                  return DropdownMenuItem(
+                    value: category,
+                    child: Text(category.name.toLowerCase(),style: GoogleFonts.aBeeZee(),),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value as category.Category;
+                  });
+                },
+              ),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_newText.text);
+                  },
+                  child: const Text("Save And Continue")),
+            ],
+          )
         ],
       ),
     );
